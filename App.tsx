0a5048ddd0a696ca7fa6cf780a684d29fc7e632c
CharlotteLaw <CharlotteLaw@berkeley.edu>
2024-10-06 15:21:34 -0700

    navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+<<<<<<< HEAD
 <<<<<<< HEAD
 import NavigationBar from '@/navigation/BottomTabNavigator';
 
@@ -12,17 +14,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HealingResources from '@/screens/HealingResources';
 import HealingCatalogue from '@/screens/HealingResources/HealingCatalogue';
 import HFHGuide from '@/screens/HealingResources/HFHGuide';
+=======
+import HealingResources from '@/HealingResources';
+>>>>>>> ba36e02 (navigation)
 import Home from '@/screens/Home';
-import LegalRights from '@/screens/LegalRights/index';
-import VideoPage from '@/screens/LegalRights/VideoPage';
+import LegalRights from '@/screens/LegalRights';
 import SeekHelp from '@/screens/SeekHelp';
-import resourceList from '@/screens/SeekHelp/ResourceList';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer independent={true}>
+<<<<<<< HEAD
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Healing Resources" component={HealingResources} />
@@ -37,6 +41,14 @@ export default function App() {
         <Stack.Screen name="Video Page" component={VideoPage} />
       </Stack.Navigator>
 >>>>>>> 0181d8b (8-healing-resource-pages (#19))
+=======
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Legal Rights" component={LegalRights} />
+        <Tab.Screen name="Healing Resources" component={HealingResources} />
+        <Tab.Screen name="Seek Help" component={SeekHelp} />
+      </Tab.Navigator>
+>>>>>>> ba36e02 (navigation)
     </NavigationContainer>
   );
 }
