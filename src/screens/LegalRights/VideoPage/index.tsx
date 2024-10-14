287297bfe372abe5a84ebf0a39862c807eb881df
Cody Garcia <czgarcia@berkeley.edu>
2024-10-13 19:25:23 -0700

    added video and testing player

--- src/screens/LegalRights/VideoPage/index.tsx
+++ src/screens/LegalRights/VideoPage/index.tsx
@@ -1,9 +1,19 @@
 import { Button, Image, Text, View } from 'react-native';
+// import PreaVideo from '@/assets/videos/da_link.mp4';
+import Video from 'react-native-video';
+import { styles } from './styles';
 
 export default function VideoPage() {
   return (
     <>
       <Text>this is video</Text>
+      <Video
+        source={require('@assets/videos/da_link.mp4')}
+        style={styles.video}
+        controls={true} // Show playback controls
+        resizeMode="contain" // Adjusts how the video scales within the player
+        paused={false} // Autoplay video
+      />
     </>
   );
 }
