1c3c904d0bf34ca7f085b0e3e768d16444a98c5d
Arfa Momin <113748174+arfamomin@users.noreply.github.com>
2024-10-24 17:03:48 -0700

    8-healing-resource-pages (#19)
    * [feat] resources rendered and scrolling by buttons
    
    * good work arfa, minor adjustments
    
    ---------
    
    Co-authored-by: philipye314 <philipye314@gmail.com>

---
+++ src/screens/HealingResources/HealingCatalogue/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { getHealingResourceData } from '@/supabase/queries/generalQueries';
+import { HealingResource } from '@/types/types';
+import styles from './styles';
+
+const themes = ['Breathing', 'Meditation', 'Joy', 'Resilience'];
+
+export default function HealingCatalogue() {
+  const [resources, setResources] = useState<HealingResource[]>([]);
+  const scrollViewRef = useRef<ScrollView | null>(null);
+  const [headerPositions, setHeaderPositions] = useState<number[]>([]);
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      const data = await getHealingResourceData();
+      setResources(data);
+    };
+    fetchResources();
+  }, []);
+
+  const groupedResources = themes.reduce(
+    (acc, theme) => {
+      acc[theme] = resources.filter(resource =>
+        resource.topics.includes(theme),
+      );
+      return acc;
+    },
+    {} as Record<string, HealingResource[]>,
+  );
+
+  const scrollToTheme = (index: number) => {
+    const targetY = headerPositions[index] || 0;
+    const paddingTop = 30;
+    scrollViewRef.current?.scrollTo({
+      y: targetY - paddingTop,
+      animated: true,
+    });
+  };
+
+  const onLayoutHeader =
+    (index: number) => (event: { nativeEvent: { layout: { y: number } } }) => {
+      const { y } = event.nativeEvent.layout;
+      setHeaderPositions(prevPositions => {
+        const newPositions = [...prevPositions];
+        newPositions[index] = y;
+        return newPositions;
+      });
+    };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.themesContainer}>
+        {themes.map((theme, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.themeButton}
+            onPress={() => scrollToTheme(index)}
+          >
+            <Text>{theme}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <ScrollView ref={scrollViewRef}>
+        <View style={styles.resourcesContainer}>
+          {themes.map((theme, index) => (
+            <>
+              <View
+                style={styles.themeHeader}
+                onLayout={onLayoutHeader(index)}
+                key={index}
+              >
+                <Text style={styles.themeHeaderText}>{theme}</Text>
+              </View>
+              <View style={styles.cardsContainer}>
+                {groupedResources[theme].map((resource, resourcesIndex) => (
+                  <View style={styles.resourceContainer} key={resourcesIndex}>
+                    <View style={styles.resourceCard}></View>
+                    <Text style={styles.resourceText}>
+                      {resource.text_content}
+                    </Text>
+                  </View>
+                ))}
+              </View>
+            </>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
--- src/types/types.tsx
+++ src/types/types.tsx
@@ -2,3 +2,11 @@ export interface Resource {
   summary: string;
   [key: string]: any;
 }
+
+export type HealingResource = {
+  id: number;
+  video_id: number;
+  text_content: string;
+  is_video: boolean;
+  topics: string[];
+};
