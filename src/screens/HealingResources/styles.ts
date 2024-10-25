0181d8bd0758320e6d40a5f5fdb2da42cef8c735
Arfa Momin <113748174+arfamomin@users.noreply.github.com>
2024-10-24 17:03:48 -0700

    8-healing-resource-pages (#19)
    * [feat] resources rendered and scrolling by buttons
    
    * good work arfa, minor adjustments
    
    ---------
    
    Co-authored-by: philipye314 <philipye314@gmail.com>

--- src/screens/HealingResources/styles.ts
+++ src/screens/HealingResources/styles.ts
@@ -5,6 +5,22 @@ export default StyleSheet.create({
     width: '100%',
     height: '100%',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    flexDirection: 'row',
+    columnGap: 20,
+  },
+  pagebutton: {
+    borderColor: '#e8e8e8',
+    backgroundColor: '#ffffff',
+    borderWidth: 2,
+    borderRadius: 20,
+    width: 500,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
   },
 });
