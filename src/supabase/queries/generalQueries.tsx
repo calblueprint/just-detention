1159b88b8767e52f45ee2fa5c13d5dc16b3a476a
Cody Garcia <czgarcia@berkeley.edu>
2024-10-29 19:42:57 -0700

    fixed philip's comments besides english/spanishModule's defualt usestate

--- src/supabase/queries/generalQueries.tsx
+++ src/supabase/queries/generalQueries.tsx
@@ -1,4 +1,4 @@
-import { HealingResource, Resource } from '@/types/types';
+import { HealingResource, Resource, VideoResource } from '@/types/types';
 import supabase from '../createClient';
 
 export const getHealingResourceData = async (): Promise<HealingResource[]> => {
@@ -24,4 +24,18 @@ export const getSeekHelpData = async (): Promise<Resource[]> => {
     throw new Error(`Error fetching resources: ${error.message}`);
   }
   return data as Resource[];
+};
+
+export const getPreaByLanguage = async (
+  languageBool: boolean,
+): Promise<VideoResource[]> => {
+  const { data, error } = await supabase // grab all the not spanish pages from the supabase table
+    .from('prea_page')
+    .select()
+    .eq('spanish', languageBool);
+  if (error) {
+    throw error;
+  }
+  data.sort((a, b) => a.page_number - b.page_number); // sort the array based on pages' page_number yur
+  return data;
 };
