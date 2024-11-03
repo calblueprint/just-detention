99f8a831bcbb90e93068b66be0833fa9fceefa6e
Arfa Momin <arfamomin16@gmail.com>
2024-11-03 13:42:43 -0800

    merged main

---
+++ src/screens/HealingResources/HFHGuide/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    // alignItems: 'center',
+  },
+});
