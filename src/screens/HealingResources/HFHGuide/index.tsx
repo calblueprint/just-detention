12c7b1bc17983d474e202b0b6f953c5804d0b70a
Arfa Momin <arfamomin16@gmail.com>
2024-11-03 16:02:55 -0800

    fix code style

--- src/screens/HealingResources/HFHGuide/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function HFHGuide() {
-  return (
-    <View>
-      <Text> This is the HFH page </Text>
-    </View>
-  );
-}
--- src/screens/HealingResources/HFHGuide/styles.ts
+++ src/screens/HealingResources/HFHGuide/styles.ts
@@ -2,10 +2,14 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
     backgroundColor: '#FFFFFF',
-    flexDirection: 'row',
-    // alignItems: 'center',
+  },
+  scrollView: {
+    padding: '5%',
+  },
+  text: {
+    fontFamily: 'Roboto Serif',
+    color: '#444',
   },
 });
