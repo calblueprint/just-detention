8c708ca236950d93d4c3d96920c8aed951567ec0
philipye314 <philipye314@gmail.com>
2024-11-19 19:22:08 -0800

    removed useless things

--- src/screens/HealingResources/HFHGuide/index.tsx
+++ src/screens/HealingResources/HFHGuide/index.tsx
@@ -62,7 +62,6 @@ export default function HFHGuide({
           <Text>Loading content...</Text>
         )}
       </ScrollView>
-      <View style={styles.nextButtonContainer}></View>
     </View>
   );
 }
