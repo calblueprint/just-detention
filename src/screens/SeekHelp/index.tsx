6f4c2f03753785f2acfcf4c359cd2199b152a07e
Arfa Momin <arfamomin16@gmail.com>
2024-12-05 00:10:18 -0800

    level logic and right panel styling

--- src/screens/SeekHelp/index.tsx
+++ src/screens/SeekHelp/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
-// import Check from 'src/assets/images/check_small.svg';
+import Check from 'src/assets/images/check_small.svg';
 import ResourceT from '@/components/Resource/Resource';
 import { getSeekHelpData } from '@/supabase/queries/generalQueries';
 import { Resource as ResourceType } from '@/types/types';
@@ -30,6 +30,20 @@ export default function ResourceList() {
     [],
   );
   const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
+  const [selectedJurisdiction, setSelectedJurisdiction] = useState<string[]>(
+    [],
+  );
+
+  const toggleSelection = (level: string) => {
+    setSelectedJurisdiction(prevSelected => {
+      // check if the level is already selected
+      if (prevSelected.includes(level)) {
+        return prevSelected.filter(item => item !== level);
+      } else {
+        return [...prevSelected, level];
+      }
+    });
+  };
 
   useEffect(() => {
     fetchData();
@@ -55,6 +69,32 @@ export default function ResourceList() {
     setFilteredResources(filtered);
   };
 
+  const applyJurisdictionFilter = () => {
+    let filtered = resources;
+
+    if (selectedJurisdiction.length > 0) {
+      filtered = filtered.filter(resource => {
+        // check if resource state matches any selected jurisdiction
+        return selectedJurisdiction.includes(resource.state);
+      });
+    }
+
+    if (selectedFilter) {
+      const tags = tagMapping[selectedFilter as keyof typeof tagMapping];
+      filtered = filtered.filter(resource =>
+        resource.tags
+          .split(',')
+          .some((tag: string) => tags.includes(tag.trim())),
+      );
+    }
+
+    setFilteredResources(filtered);
+  };
+
+  useEffect(() => {
+    applyJurisdictionFilter();
+  }, [selectedJurisdiction, selectedFilter, resources]);
+
   return (
     <View style={styles.container}>
       <View style={styles.leftPanel}>
@@ -63,8 +103,16 @@ export default function ResourceList() {
           <View style={styles.selectJurisdictionContainer}>
             {jurisdiction.map(level => (
               <View key={level} style={styles.checkButtonContainer}>
-                <TouchableOpacity style={styles.checkButton}>
-                  {/* <Check /> */}
+                <TouchableOpacity
+                  style={[
+                    styles.checkButton,
+                    selectedJurisdiction.includes(level)
+                      ? styles.selectedCheckButton
+                      : null,
+                  ]}
+                  onPress={() => toggleSelection(level)}
+                >
+                  {selectedJurisdiction.includes(level) && <Check />}
                 </TouchableOpacity>
                 <Text style={styles.buttonText}>{level}</Text>
               </View>
@@ -100,26 +148,28 @@ export default function ResourceList() {
       </View>
 
       <ScrollView style={styles.rightPanel}>
-        {filteredResources.length > 0 ? (
-          filteredResources
-            .sort(function (a, b) {
-              return a.org_name.localeCompare(b.org_name);
-            })
-            .map((resource, index) => (
-              <ResourceT
-                key={index}
-                org_name={resource.org_name}
-                summary={resource.summary}
-                address={resource.address}
-                phone_number={resource.phone_number}
-                office_hours={resource.office_hours}
-                tags={resource.tags}
-                hotline_number={resource.hotline_number}
-              />
-            ))
-        ) : (
-          <Text>No resources found</Text>
-        )}
+        <View style={styles.resourcesContainer}>
+          {filteredResources.length > 0 ? (
+            filteredResources
+              .sort(function (a, b) {
+                return a.org_name.localeCompare(b.org_name);
+              })
+              .map((resource, index) => (
+                <ResourceT
+                  key={index}
+                  org_name={resource.org_name}
+                  summary={resource.summary}
+                  address={resource.address}
+                  phone_number={resource.phone_number}
+                  office_hours={resource.office_hours}
+                  tags={resource.tags}
+                  hotline_number={resource.hotline_number}
+                />
+              ))
+          ) : (
+            <Text style={styles.buttonText}>No resources found</Text>
+          )}
+        </View>
       </ScrollView>
     </View>
   );
